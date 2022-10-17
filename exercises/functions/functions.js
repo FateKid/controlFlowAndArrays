7ed348fcc402d25e204074ea17fa01ca25b0dcb3
charlesK <115595915+charlesknapp@users.noreply.github.com>
2022-10-17 11:59:13 -0600

    Modified functions js with new methods

--- exercises/functions/functions.js
+++ exercises/functions/functions.js
@@ -32,3 +32,38 @@ const increaseAge = (person) => {
 increaseAge(profileInfo);
 
 ////////////////////////////////////////////////////////////////
+
+function areBothEven(n1, n2) {
+    return !(n1 % n2) && !(n2 % 2);
+}
+console.log*(areBothEven(3, 7));
+
+//let maria = getDevObject("Maria","HTML","CSS","JavaScript")
+
+let maria2 = {
+    name: "Maria",
+    skills: []
+}
+
+//////////////////////////////////////- [ Function with 1 param ]
+
+const sayName = (name) => {
+    //console.log("Hello, my name is " + name); //concatitnation
+    console.log (`Hello, my name is ${name}`) //direct injection
+}
+
+sayName("Kadija")
+
+
+//Calcuating the area of a circle
+const calcAreaOfCircle = () => {
+    let promptRadius = prompt("What is the radius of your circle? \t");
+
+    let circleArea = Math.PI * promptRadius * promptRadius;
+    console.log("The area of this circle is " + circleArea)
+}
+
+calcAreaOfCircle();
+
+
+////////////////////////////////////////////////////////////////
