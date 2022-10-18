926a3a605495e0126a410c69589cfa15a6be5568
charlesK <115595915+charlesknapp@users.noreply.github.com>
2022-10-18 10:18:09 -0600

    Re-structured the content

---
+++ exercises/functions/app.js
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////////////////////// - [Car Information Object]
+
+const myCar = {
+    make: "Infiniti",
+    model: "G37",
+    year: 2012,
+    nickName: "moneyWaste",
+    isLoud: true,
+    isRunning: false
+}
+console.log("Your Car Information:")
+console.log(` Make: ${myCar.make} \n Model: ${myCar.model} \n Year: ${myCar.year}`)
+console.log(`Active Parameters:\n IsRunning? ${myCar.isRunning} \n IsLoud? ${myCar.isLoud}`)
+
+////////////////////////////////////////////////////////////////////// - [Pet Information Object]
+
+const myPet = {
+    name: "Rouge",
+    age: 10,
+    favorites: ['Outside', ' potty time', ' food'],
+    canFrench: "cannot",
+    canRubik: "cannot",
+    bellyRubs: "will"
+}
+console.log(`My dog is named ${myPet.name}
+\nShe is ${myPet.age} years old.
+\nand loves ${myPet.favorites}.
+\nShe ${myPet.canFrench} speak French.
+\nShe ${myPet.canRubik} solve the Rubik Cube.
+\nShe ${myPet.bellyRubs} accept belly rubs.`)
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
